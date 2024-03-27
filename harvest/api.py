185b381ea0448ee5dae2bb0a78ead570e2000f81
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-03-27 11:48:07 -0400

    added ProcessStatusCodes
    added __enter__ __exit__

--- harvest/api.py
+++ harvest/api.py
@@ -4,6 +4,8 @@ from typing import Any
 from urllib.request import getproxies
 from requests import Request, Session
 from logging import getLogger
+from processes import ProcessStatusCodes
+
 logger = getLogger('harvest')
 
 
@@ -22,7 +24,17 @@ class HarvestRequest(Request):
 
         super().__init__(url=url, method=method.upper(), params=params, json=str_json)
 
+        self.status = ProcessStatusCodes.INITIALIZED
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return None
+
     def query(self) -> dict or tuple:
+        self.status = ProcessStatusCodes.RUNNING
+
         prepared_statement = self.prepare()
 
         session = Session()
@@ -38,20 +50,26 @@ class HarvestRequest(Request):
 
         # TODO: handle urllib3 connection errors
         except ConnectionError as ce:
+            self.status = ProcessStatusCodes.ERROR
+
             from exceptions import BaseHarvestException
             BaseHarvestException(*ce.args, log_level='error')
             return 400, 'Could not make connection to api.'
 
-        logger.debug(f'{prepared_statement}[{response.status_code}')
-
-        if 200 <= response.status_code <= 299:
-            return response.json()
-
         else:
-            from messages import add_message
-            add_message(__name__, 'WARN', response.status_code, response.reason)
+            logger.debug(f'{prepared_statement}[{response.status_code}')
 
-            return response.status_code, response.reason
+            if 200 <= response.status_code <= 299:
+                return response.json()
+
+            else:
+                from messages import add_message
+                add_message(__name__, 'WARN', response.status_code, response.reason)
+
+                return response.status_code, response.reason
+
+        finally:
+            self.status = ProcessStatusCodes.COMPLETE
 
 
 def get_auth(method: str) -> tuple:
