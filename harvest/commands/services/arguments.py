3649816cc1cce4018e6190052f9794e7dfc99335
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-04-01 09:12:35 -0400

    added services kill

--- harvest/commands/services/arguments.py
+++ harvest/commands/services/arguments.py
@@ -12,10 +12,18 @@ subparser = parser.add_subparsers()
 services_attach_parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter)
 services_attach_parser.add_argument('name',
                                     completer=services_completer.run,
-                                    help='The name of a process')
+                                    help='The name of a process.')
 services_attach_parser.add_argument('--show-subtasks',
                                     action='store_true',
                                     help='Show individual subtask progress bars.')
+
+# services kill
+services_kill_parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter)
+services_kill_parser.add_argument('names',
+                                  nargs='*',
+                                  completer=services_completer.run,
+                                  help='One or more processes to terminate.')
+
 
 # services list
 services_list_parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter)
--- harvest/commands/services/commandset.py
+++ harvest/commands/services/commandset.py
@@ -1,5 +1,6 @@
 from cmd2 import with_default_category, CommandSet, with_argparser, as_subcommand_to
-from .arguments import parser, services_attach_parser, services_list_parser
+from processes import ConcurrentProcesses
+from .arguments import parser, services_attach_parser, services_kill_parser, services_list_parser
 
 
 @with_default_category('Harvest')
@@ -11,7 +12,6 @@ class ServicesCommand(CommandSet):
 
     @as_subcommand_to('services', 'attach', services_attach_parser, help='Attach a progress bar to a running job.')
     def attach(self, args):
-        from processes import ConcurrentProcesses
         from text.printing import print_message
 
         for process in ConcurrentProcesses.objects:
@@ -19,11 +19,26 @@ class ServicesCommand(CommandSet):
                 process.attach_progressbar()
 
             else:
-                print_message(text=f'No eligible running processes named `{args.name}`.', color='INFO', as_feedback=True)
+                print_message(text=f'No eligible running processes named `{args.name}`.',
+                              color='INFO',
+                              as_feedback=True)
+
+    @as_subcommand_to('services', 'kill', services_kill_parser, help='Stop jobs.')
+    def kill(self, args):
+        from text.printing import print_message
+
+        for process in ConcurrentProcesses.objects:
+            if process.name in args.names and hasattr(process, 'kill'):
+                process.kill()
+                print_message(text=f'Sent kill command to `{process.name}`.', color='WARN', as_feedback=True)
+
+            else:
+                print_message(text=f'No eligible running processes named `{args.names}`.',
+                              color='INFO',
+                              as_feedback=True)
 
     @as_subcommand_to('services', 'list', services_list_parser, help='Get a list of running jobs')
     def list(self, args):
-        from processes import ConcurrentProcesses
         from text.printing import print_data
 
         keys, data = ConcurrentProcesses().report()
