01fdfdd7f1ef23a4de7d23fb7fcce37e60d24f03
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-03-31 20:04:41 -0400

    removed unused code
    added get_subtask()

--- harvest/commands/base.py
+++ harvest/commands/base.py
@@ -1,8 +1,30 @@
-from cmd2 import (
-    Cmd,
-    CommandSet,
-    CompletionError,
-    Statement,
-    with_category,
-    with_default_category,
-)
+from argparse import Namespace
+from cmd2 import Cmd2ArgumentParser, CommandSet
+
+
+def get_subtask(parent: CommandSet, parser: Cmd2ArgumentParser, args: Namespace):
+    """
+    A routine for selecting the appropriate subtask in a CommandSet to run.
+    Args:
+        parent: The CommandSet calling this function.
+        parser: The command's argument parser.
+        args: The parsed arguments.1
+
+    Returns:
+        None
+    """
+
+    argv = args.cmd2_statement._Cmd2AttributeWrapper__attribute.argv
+
+    try:
+        command = argv[1]
+
+    except IndexError:
+        parser.print_help()
+
+    else:
+        if hasattr(parent, command):
+            getattr(parent, command)(args)
+
+        else:
+            parser.print_help()
