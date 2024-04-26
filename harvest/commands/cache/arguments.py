c935c660e963af9750ef6ba9ba085fb86f61c083
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-04-26 10:18:47 -0400

    added AvailablePstarRemoteCompleter()

--- harvest/commands/cache/arguments.py
+++ harvest/commands/cache/arguments.py
@@ -4,6 +4,15 @@ from cmd2 import Cmd
 from commands.arguments.parts import format_parser, pstar_parser_optional, thread_parser, yes_parser
 from rich_argparse import RawTextRichHelpFormatter
 
+# completers
+from .completers import AvailablePstarRemoteCompleter
+platform_remote_completer = AvailablePstarRemoteCompleter(path='/cache/get/pstar_dimensions',
+                                                          remote_api_kwargs={'dimension': 'platform'})
+service_remote_completer = AvailablePstarRemoteCompleter(path='/cache/get/pstar_dimensions',
+                                                         remote_api_kwargs={'dimension': 'service'})
+type_remote_completer = AvailablePstarRemoteCompleter(path='/cache/get/pstar_dimensions',
+                                                      remote_api_kwargs={'dimension': 'type'})
+
 # base parser
 parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter)
 subparser = parser.add_subparsers()
@@ -15,13 +24,13 @@ collect_parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter,
 # cache map
 map_parser = Cmd2ArgumentParser(formatter_class=RawTextRichHelpFormatter)
 
-map_parser.add_argument('platform', type=str,
+map_parser.add_argument('platform', type=str, completer=platform_remote_completer.run,
                         help='Set the platform for the data.'
                              ' Example: `aws`')
-map_parser.add_argument('service', type=str,
+map_parser.add_argument('service', type=str, completer=service_remote_completer.run,
                         help='Set the service name for the data.'
                              ' Example: `rds`')
-map_parser.add_argument('type', type=str,
+map_parser.add_argument('type', type=str, completer=type_remote_completer.run,
                         help='Set the service subtype for the data.'
                              ' Example: instance')
 map_parser.add_argument('--account', type=str,
--- harvest/commands/cache/completers.py
+++ harvest/commands/cache/completers.py
@@ -1,2 +1,12 @@
 from typing import List
-from commands.arguments.completers import BaseCompleter
+from commands.arguments.completers import RemoteBaseCompleter
+
+
+class AvailablePstarRemoteCompleter(RemoteBaseCompleter):
+    def _run(self, *args, **kwargs) -> List[str]:
+        try:
+            return self.result.get('result')
+
+        except Exception as ex:
+            return []
+
