997e7545e8c112de7abb2341014f11703022d117
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-13 09:51:14 -0400

    now references meta.py

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
+from CloudHarvestCLI.meta import meta
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-config = dict(name='CloudHarvestCLI',
-              version='0.1.0',
-              description='This is the Command Line Interface for CloudHarvest.',
-              author='Cloud Harvest, Fiona June Leathers',
-              url='https://github.com/Cloud-Harvest/CloudHarvestCLI',
-              packages=find_packages(include=['CloudHarvestCLI']),
-              install_requires=requirements,
-              classifiers=[
-                  'Programming Language :: Python :: 3.12',
-              ])
+config = dict(packages=find_packages(include=['CloudHarvestCLI']),
+              install_requires=requirements)
+
+
+config = config | meta
 
 
 def main():
