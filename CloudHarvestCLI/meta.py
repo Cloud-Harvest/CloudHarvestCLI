fbe18addddc968a0a99551793389e7241cc4683e
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-15 15:44:11 -0400

    added file

---
+++ CloudHarvestCLI/meta.py
@@ -0,0 +1,10 @@
+meta = dict(
+    name='CloudHarvestCLI',
+    version='0.1.0',
+    description='This is the Command Line Interface for CloudHarvest.',
+    author='Cloud Harvest, Fiona June Leathers',
+    url='https://github.com/Cloud-Harvest/CloudHarvestCLI',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+    ]
+)
