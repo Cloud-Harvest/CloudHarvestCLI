1ec48f2284d5592cc8868ce2c1595ff941ddbac9
Fiona June Leathers <fiona.june.leathers@outlook.com>
2025-03-20 20:30:49 -0400

    Fixed an issue with `input_pick_choices()` where it did not populate the correct list of choices

--- CloudHarvestCLI/text/inputs.py
+++ CloudHarvestCLI/text/inputs.py
@@ -4,7 +4,7 @@ from CloudHarvestCLI.text.styling import TextColors
 from typing import List
 
 
-def input_pick_choices(prompt: str, data: List[dict], keys: list) -> str:
+def input_pick_choices(prompt: str, data: List[dict], keys: list, selection_keys: list = None, record_index_identifier: str = 'i') -> str:
     """
     This function is used to display a list of choices to the user and return the user's selection.
 
@@ -12,14 +12,27 @@ def input_pick_choices(prompt: str, data: List[dict], keys: list) -> str:
     prompt (str): The question that is displayed to the user.
     data (List[dict]): The data that is displayed to the user. Each dictionary in the list represents a record.
     keys (list): The keys that are used to display the data. Each key represents a column in the displayed table.
+    selection_keys (list, optional): The keys that are used to populate the choices list. Default is None. When None,
+                                     the function will use the keys parameter instead.
+    record_index_identifier (str, optional): The key that is used to identify each record. Default is 'i'.
 
     Returns:
     str: The user's selection.
     """
     from CloudHarvestCLI.text.printing import print_data
-    print_data(data=data, keys=keys, output_format='table', record_index_keyname='i')
+    print_data(data=data, keys=keys, output_format='table', record_index_keyname=record_index_identifier)
 
-    return Prompt().ask(prompt=Text(prompt, style=TextColors.PROMPT), choices=keys)
+    choices = [
+        record.get(key)
+        for key in selection_keys or keys
+        for record in data
+        if record.get(key) is not None
+    ]
+
+    # Add the record index to the choices list
+    choices += [str(i) for i in range(len(data))]
+
+    return Prompt().ask(prompt=Text(prompt, style=TextColors.PROMPT), choices=choices, show_choices=False)
 
 
 def input_boolean(prompt: str, yes_argument: bool = False) -> bool:
