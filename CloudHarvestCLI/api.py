109baa854ac11dff744f622a4122881201c242ba
Fiona June Leathers <fiona.june.leathers@outlook.com>
2025-06-13 10:31:08 -0400

    adjusted retry delay

--- CloudHarvestCLI/api.py
+++ CloudHarvestCLI/api.py
@@ -142,7 +142,7 @@ def request(request_type: HTTP_REQUEST_TYPES, endpoint: str, data: dict = None, 
                     from time import sleep
 
                     logger.debug(f'request:{request_id}: Retrying ({attempt + 1}/{retries})...')
-                    sleep(2 ** attempt)  # Exponential backoff
+                    sleep(1)  # Exponential backoff
                     continue
 
                 else:
