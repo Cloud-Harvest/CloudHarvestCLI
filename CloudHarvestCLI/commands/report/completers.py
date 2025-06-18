ff1ee184fb13f95f2ac5506ed7c6363c3f27c751
Fiona June Leathers <fiona.june.leathers@outlook.com>
2025-06-18 14:43:13 -0400

    ReportNameCompleter will not list hidden templates in the report output

--- CloudHarvestCLI/commands/report/completers.py
+++ CloudHarvestCLI/commands/report/completers.py
@@ -8,9 +8,10 @@ class ReportNameCompleter(RemoteBaseCompleter):
     def _run(self, *args, **kwargs) -> List[str]:
         try:
             results = [
-                str(template).replace('template_reports/', '')
+                str(template).replace('template_reports/', '')  # Removes the prefix to return just the template name
                 for template in self.result['result']
-                if str(template).startswith('template_reports/')
+                if str(template).startswith('template_reports/')            # Must be a report template
+                   and not str(template.split('.')[-1]).startswith('_')     # Must not be a hidden template
             ]
 
             return results
